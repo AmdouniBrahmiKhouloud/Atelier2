fafc15f22574026e3b57895878cf8f1ff72f1a75
AmdouniBrahmiKhouloud <khouloud.amdounibrahmi@esprit.tn>
2021-10-07 00:56:41 +0100

    Tp complite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FournisseurComponentComponent } from './fournisseur-component/fournisseur-component.component';
+import { ListFournisseurComponentComponent } from './list-fournisseur-component/list-fournisseur-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FournisseurComponentComponent,
+    ListFournisseurComponentComponent
   ],
   imports: [
     BrowserModule
